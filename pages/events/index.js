2a239a4f37c2b057be5ebfc591defdf7c75894e2
tatsuro1997 <minagawa@dptr.jp>
2022-05-24 17:02:12 +0900

    Adding Filtered Events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,22 @@
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventsSearch />
-      <EventList items={events}/>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
     </>
   );
 }
