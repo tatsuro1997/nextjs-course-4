d216dc027bdf6985364bd1243eadd073df0cc8ed
tatsuro1997 <minagawa@dptr.jp>
2022-05-26 14:10:06 +0900

    Add Error Handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,19 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://tatz:eB!fyXeYShek!2P@cluster0.smxrn.mongodb.net/events?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -9,14 +23,26 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://tatz:eB!fyXeYShek!2P@cluster0.smxrn.mongodb.net/events?retryWrites=true&w=majority"
-    );
-    const db = client.db();
+    let client;
 
-    await db.collection("newsletter").insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({
+        message: "Connecting to the database failed!",
+      });
+      return;
+    }
 
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({
+        message: "Inserting data failed!",
+      });
+      return;
+    }
 
     res.status(201).json({ message: "Signed up!" });
   }
